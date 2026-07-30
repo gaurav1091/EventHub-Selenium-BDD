89f3b6ba2b1107f4b00feeeb86aae02df3f1635d
Gaurav Arora <gauravarora1091@gmail.com>
2026-07-31 00:40:48 +0530

    Moved retry wiring into the strongly typed cucumber runners.

--- src/test/java/com/eventhub/automation/runners/ParallelCucumberTestRunner.java
+++ src/test/java/com/eventhub/automation/runners/ParallelCucumberTestRunner.java
@@ -1,8 +1,12 @@
 package com.eventhub.automation.runners;
 
+import com.eventhub.automation.listeners.RetryAnalyzer;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -21,6 +25,16 @@ import org.testng.annotations.DataProvider;
         monochrome = true
 )
 public class ParallelCucumberTestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @Test(
+            groups = "cucumber",
+            description = "Runs Cucumber Scenarios",
+            dataProvider = "scenarios",
+            retryAnalyzer = RetryAnalyzer.class)
+    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+        super.runScenario(pickleWrapper, featureWrapper);
+    }
+
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
